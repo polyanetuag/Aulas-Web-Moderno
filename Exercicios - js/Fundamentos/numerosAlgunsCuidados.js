8ab6f7fdc2adffc2be1b40b04079851b96026636
PolyaneTuag <pollyportugues@hotmail.com>
2020-05-28 17:56:55 -0300

    Adição de exemplos

--- Exercicios - js/Fundamentos/numerosAlgunsCuidados.js
+++ Exercicios - js/Fundamentos/numerosAlgunsCuidados.js
@@ -1,5 +1,7 @@
 console.log(7 / 0)  // Quanto mais se aproxima de zero, o valor que é dividido vai aumentando(Infinity)
 console.log('10'/ 2)
+console.log('3' + 2)    // efeito de concatenação(a string ganha!)
+console.log('3' - 2)
 console.log('Show!' * 2)  // Não é um número
 console.log(0.1 + 0.7)    // Lentidão na precisão dos números(imprecisão)
 //  console.log(10.toString())    ERRO
