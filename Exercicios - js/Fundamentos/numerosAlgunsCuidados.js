000866f38ba51164103315384df7fc258ea08180
PolyaneTuag <pollyportugues@hotmail.com>
2020-05-28 12:34:36 -0300

    Numeros Cuidados

---
+++ Exercicios - js/Fundamentos/numerosAlgunsCuidados.js
@@ -0,0 +1,6 @@
+console.log(7 / 0)  // Quanto mais se aproxima de zero, o valor que é dividido vai aumentando(Infinity)
+console.log('10'/ 2)
+console.log('Show!' * 2)  // Não é um número
+console.log(0.1 + 0.7)    // Lentidão na precisão dos números(imprecisão)
+//  console.log(10.toString())    ERRO
+console.log((10.345).toFixed(2))
