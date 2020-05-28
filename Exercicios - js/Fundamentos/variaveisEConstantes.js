894f36885f498557caaf9491c697d0d0fc718e38
PolyaneTuag <pollyportugues@hotmail.com>
2020-05-28 10:57:57 -0300

    O básico de var, let e const

---
+++ Exercicios - js/Fundamentos/variaveisEConstantes.js
@@ -0,0 +1,15 @@
+var a = 3
+let b = 4   //  'let'- uso mais moderno
+
+var a = 30
+b = 40
+
+console.log(a, b)
+
+a = 300
+b = 400
+console.log(a, b)
+
+const c = 5
+//c = 50      // a constante não muda de valor 
+console.log(c)
