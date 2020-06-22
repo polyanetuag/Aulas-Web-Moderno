8226e0d41cfb324048327132d0fd865cce8cd289
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-22 10:01:04 -0300

    "This" pode Variar

---
+++ Exercicios - js/Função/thisPodeVariar.js
@@ -0,0 +1,9 @@
+// função this: forma de referenciar o objeto atual daquela execução
+
+// Em JS, o this pode variar dependendo de como a função é chamada (em sua forma tradicional)
+
+/* arrow function (=>): forma para reduzir a sintaxe e mostrar uma forma invariável do this 
+
+const f3 = () => console.log(this === window) */
+
+// com a => , o this não varia. Ele será definido no momento que a função foi definida
