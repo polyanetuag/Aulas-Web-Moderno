7c139f37e6742a0d4201ff9e06a1346a1a299677
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-23 17:44:05 -0300

    Classe vs Factory - (This e Arrow Function)

---
+++ Exercicios - js/Função/classeVsFactory.js
@@ -0,0 +1,25 @@
+//Usando o this - problemas no browser(indefined), devido a ser variável
+
+class Pessoa {
+    constructor(nome) {
+        this.nome = nome
+    }
+
+    falar() {
+        console.log(`Meu nome é ${this.nome}`)
+    }
+}
+
+const p1 = new Pessoa ('João')
+p1.falar()
+
+//Usando uma arrow function
+
+const criarPessoa = nome => {
+    return {
+        falar: () => console.log(`Meu nome é ${nome}`)
+    }
+}
+
+const p2 = criarPessoa('João')
+p2.falar()
