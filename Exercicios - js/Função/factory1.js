4d60112346ed5eef4b016e4a5891e376584acdd6
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-23 15:46:56 -0300

    Factory - Função que retorna um objeto

---
+++ Exercicios - js/Função/factory1.js
@@ -0,0 +1,14 @@
+// Função que retorna um objeto
+
+//Factory simples
+
+function criarPessoa() {
+    return {
+        nome: 'Ana',
+        sobrenome: 'Silva'
+    }
+}
+
+console.log(criarPessoa())
+
+
