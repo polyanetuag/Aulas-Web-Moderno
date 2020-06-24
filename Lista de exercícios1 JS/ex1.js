4ccd2f990c6c7db9a4a5b41ce0fee6634cb1ea1f
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-24 17:43:12 -0300

    Operações Aritméticas

---
+++ Lista de exercícios1 JS/ex1.js
@@ -0,0 +1,22 @@
+function soma(x, y) {
+    return x + y
+}
+    console.log(soma(5, 6))
+
+
+function subtracao(x, y) {
+    return x - y
+}
+    console.log(subtracao(5, 6))
+
+
+function multiplicacao (x, y) {
+    return x * y
+}
+    console.log(multiplicacao(5, 6))
+
+
+function divisao(x, y) {
+    return x / y
+}
+    console.log(divisao(5, 6))
