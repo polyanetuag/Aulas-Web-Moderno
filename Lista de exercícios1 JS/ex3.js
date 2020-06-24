111a9ffd72d44bec88cf421256a831618e9a324e
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-24 17:46:21 -0300

    Exponenciação

---
+++ Lista de exercícios1 JS/ex3.js
@@ -0,0 +1,10 @@
+function matematica(base, expoente) {
+    let resultado = base ** expoente
+    return resultado
+}
+
+console.log(matematica(2,0))
+console.log(matematica(2,1))
+console.log(matematica(2,2))
+console.log(matematica(2,3))
+console.log(matematica(2,4))
