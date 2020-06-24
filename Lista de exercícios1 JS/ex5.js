61b388a0394189332c8a71dbd1b3b378d8c025cd
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-24 17:49:57 -0300

    Resultado em forma de moeda

---
+++ Lista de exercícios1 JS/ex5.js
@@ -0,0 +1,10 @@
+let soma = function dinheiroSoma(v1, v2){
+    return (v1 + v2).toFixed(2)
+    
+}
+
+console.log(`R$ ${soma(0.1, 0.2)}`)
+console.log(`R$ ${soma(0.2, 0.5)}`)
+console.log(`R$ ${soma(0.4, 0.4)}`)
+console.log(`R$ ${soma(0.2, 0.2)}`)
+console.log(`R$ ${soma(0.3, 0.3)}`)
