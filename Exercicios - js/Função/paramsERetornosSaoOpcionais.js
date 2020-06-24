154cd7784c90c4d534972d06102919d5452c8e6c
PolyaneTuag <pollyportugues@hotmail.com>
2020-06-24 11:17:43 -0300

    Parâmetros e Retornos são opcionais

--- Exercicios - js/Função/paramsERetornosSaoOpcionais.js
+++ Exercicios - js/Função/paramsERetornosSaoOpcionais.js
@@ -3,7 +3,7 @@
 function area(largura, altura) {
     const area = largura *altura
     if(area > 20) {
-        console.log(`Valor aciam do permitido: ${area}m2.`)
+        console.log(`Valor acima do permitido: ${area}m2.`)
     } else {
         return area 
     }
