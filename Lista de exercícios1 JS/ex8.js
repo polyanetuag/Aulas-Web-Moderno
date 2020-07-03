b7cf158d96e6c14318e012be289dcb2733c325d3
PolyaneTuag <pollyportugues@hotmail.com>
2020-07-03 15:49:45 -0300

    Correção

--- Lista de exercícios1 JS/ex8.js
+++ Lista de exercícios1 JS/ex8.js
@@ -1,26 +1,24 @@
 let pontuacao = "10 20 20 8 25 3 0 30 1"
 
-let listaPontos = pontuacao.split(' ')
+let listaPontos = pontuacao.split(' ')      // separa a string
 console.log(listaPontos)
 
-pontuacaoMaxima = 0
-pontuacaoMinima = 100
+let pontuacaoMaxima = [0]
+let pontuacaoMinima = [0]
+let piorJogo = 1
+let recordes = 0
 
-function jogos() {
-    for(let pontuacao of listaPontos) {
-        if(pontuacaoMaxima <= pontuacao) {
-            pontuacaoMaxima = pontuacao
-            += 1
-            console.log('Quebrou o recorde')
+function jogos(pontuacao) {
+    for(let i =1; i < listaPontos.length; i++) {
+        if(listaPontos[i] > pontuacaoMaxima) {
+            pontuacaoMaxima = listaPontos[i]
+            recordes++
+        }else if (listaPontos[i] < pontuacaoMinima) {
+            pontuacaoMinima = listaPontos[i]
+            piorJogo = i+1;
         }
     } 
+    return [recordes, piorJogo]
 }
 
-jogos()
-
-//PontuacaoMaxima = 0
-
-// Para cada pontuçao faça
-//     Se pontuacaMaxima < pontucao
-//         pontucaoMaxima = pontuacao
-//         quebrourecorde += 1
+console.log(jogos(pontuacao))
