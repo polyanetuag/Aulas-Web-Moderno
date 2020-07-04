4485ee4e93cefbc37880b9a97a062067e3d83f48
PolyaneTuag <pollyportugues@hotmail.com>
2020-07-04 17:25:57 -0300

    Introdução à OO

---
+++ objeto/oo.js
@@ -0,0 +1,22 @@
+// Código não executável
+
+// Procedural - função que manipula dados e retorna um valor 
+processamento(valor1, valor2, valor3)
+
+// OO - objetos que tem atributos e funções (cápsula)
+objeto = {
+    valor1,
+    valor2,
+    valor3,
+    processamento() {
+        // ...
+    }
+}
+
+objeto.processamento()   // Foco passou a ser o objeto
+
+// Principios Importantes:
+// 1. Abstração - Simplificação do objeto para dentro do sistema
+// 2.Encapsulamento - Detalhes embutidos, interface mais enxuta
+// 3. Herança(prototype) - recebe atributos/caracteristicas de pais
+// 4. Polimosfismo - múltiplas formas de atribuição
