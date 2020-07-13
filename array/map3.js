65da1bdd5dbb1b0bf8696144d6c753140b35b20f
Polyane P. Tuag <pollyportugues@hotmail.com>
2020-07-13 17:58:44 -0300

    Array - Função map3 (implementação)

---
+++ array/map3.js
@@ -0,0 +1,23 @@
+Array.prototype.map2 = function(callback) {
+    const newArray = []
+    for (let i = 0; i < this.length; i++) {
+        newArray.push(callback(this[i], i , this))
+    }
+    return newArray
+}
+
+
+const carrinho = [
+    '{"nome": "Borracha", "preco": 3.45 }',
+    '{"nome": "Caderno", "preco": 13.90 }',
+    '{"nome": "Kit de lápis", "preco": 41.22 }',
+    '{"nome": "Caneta", "preco": 7.50 }'
+]
+
+// Retornar um array apenas com os precos 
+
+const paraObjeto = json => JSON.parse(json)             // transformar a string em objeto
+const apenasPreco = produto => produto.preco            // retorna somente os preços
+
+const resultado = carrinho.map2(paraObjeto).map2(apenasPreco)
+console.log(resultado)
