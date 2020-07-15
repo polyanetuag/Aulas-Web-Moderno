daaadc2269d5f87637eb912da40c70bfeaf83ee2
Polyane P. Tuag <pollyportugues@hotmail.com>
2020-07-15 19:52:34 -0300

    Método Concat

---
+++ array/concat.js
@@ -0,0 +1,8 @@
+// Método concat - concatenar com arrays
+
+const filhas = ['Ualeska', 'Cibalena']
+const filhos = ['Uoxiton', 'Uesclei']
+const todos = filhas.concat(filhos)
+console.log(todos, filhas, filhos)
+
+console.log([].concat([1, 2], [3, 4], 5, [[6, 7]]))
