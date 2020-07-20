6af242476812f21a2f0e13fa6a0082aff567f36f
Polyane P. Tuag <pollyportugues@hotmail.com>
2020-07-20 19:56:56 -0300

    Construção de um banco de dados fake

---
+++ Exercicios - js/node/projeto/src/bancoDeDados.js
@@ -0,0 +1,22 @@
+const sequence = {
+    _id: 1,
+    get id() { return this._id++ }          // retorna o próximo valor de id 
+}
+
+const produtos = {}
+
+function salvarProduto(produto) {
+    if (!produto.id) produto.id = sequence.id           // se o produto não tiver setado, ele adiciona com um id
+    produtos[produto.id] = produto
+    return produto
+}
+
+function getProduto(id) {
+    return produtos[id] || {}               // caso não encontre um id, retorna um objeto vazio
+}
+
+function getProdutos() {                    // retorna todos os valores do objeto
+    return Object.values(produtos)
+}
+
+module.exports = { salvarProduto, getProduto, getProdutos }
