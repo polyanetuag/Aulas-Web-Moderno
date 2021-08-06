6b9a1fe55d8e9300ec6115d5baa8cb919b980d04
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-06 15:57:08 -0300

    excluding test

--- Exercicios - js/ajax/server.js
+++ Exercicios - js/ajax/server.js
@@ -8,8 +8,4 @@ app.use(express.static("."));
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(bodyParser.json());
 
-//requisições
-
-app.get("/teste", (req, res) => res.send("OK"));
-
 app.listen(8080, () => console.log("Executando..."));
