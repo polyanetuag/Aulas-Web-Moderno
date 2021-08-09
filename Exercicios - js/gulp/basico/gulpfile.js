7e7ade748131681dcb65399ebd06f1ac84fec478
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-09 19:04:43 -0300

    copying files via gulp

--- Exercicios - js/gulp/basico/gulpfile.js
+++ Exercicios - js/gulp/basico/gulpfile.js
@@ -1,9 +1,26 @@
 const gulp = require("gulp");
 const series = gulp.series;
 
+const antes1 = (cb) => {
+  console.log("Tarefa antes 1");
+  return cb();
+};
+
+const antes2 = (cb) => {
+  console.log("Tarefa antes 2");
+  return cb();
+};
+
 function copiar(cb) {
-  console.log("Tarefa de copiar");
+  gulp
+    .src(["pastaA/arquivo1.txt", "pastaA/arquivo2.txt"])
+    .pipe(gulp.dest("pastaB")); //transformações no arquivo de entrada
   return cb();
 }
 
-module.exports.default = series(copiar);
+const fim = (cb) => {
+  console.log("Tarefa fim");
+  return cb();
+};
+
+module.exports.default = series(antes1, antes2, copiar, fim);
