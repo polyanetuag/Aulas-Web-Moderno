0291ccb9116301caea833701643d18895cbca3b0
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-11 20:12:54 -0300

    html build

--- Exercicios - js/gulp/css/gulpfile.js
+++ Exercicios - js/gulp/css/gulpfile.js
@@ -13,4 +13,8 @@ function transformacaoCSS() {
     .pipe(gulp.dest("build/css"));
 }
 
-exports.default = series(transformacaoCSS);
+function copiarHTML() {
+  return gulp.src("src/index.html").pipe(gulp.dest("build"));
+}
+
+exports.default = series(transformacaoCSS, copiarHTML);
