4a4323824dac211c67ca859dfbff7dbdbcf932ae
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-13 17:22:48 -0300

    minifying html files

--- Exercicios - js/gulp/spa/gulpTasks/app.js
+++ Exercicios - js/gulp/spa/gulpTasks/app.js
@@ -1,7 +1,16 @@
 const gulp = require("gulp");
+const babel = require("gulp-babel");
+const uglify = require("gulp-uglify");
+// const sass = require("gulp-sass");
+const uglifycss = require("gulp-uglifycss");
+const concat = require("gulp-concat");
+const htmlmin = require("gulp-htmlmin");
 
-function appHTML(cb) {
-  return cb();
+function appHTML() {
+  return gulp
+    .src("src/**/*.html")
+    .pipe(htmlmin({ collapseWhitespace: true }))
+    .pipe(gulp.dest("build"));
 }
 
 function appCSS(cb) {
