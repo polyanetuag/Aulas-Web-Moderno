f1c49d7650e262f893abd40469f9e02b974fe2e8
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-14 15:50:33 -0300

    configuring output folder

--- Exercicios - js/webpack/webpack.config.js
+++ Exercicios - js/webpack/webpack.config.js
@@ -3,4 +3,8 @@ const webpack = require("webpack");
 module.exports = {
   mode: "development",
   entry: "./src/principal.js",
+  output: {
+    filename: "principal.js",
+    path: __dirname + "/public",
+  },
 };
