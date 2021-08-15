fd67fbe87c5aae14b396a44063895e454da49d88
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-15 17:16:32 -0300

    configuring optimization

--- Exercicios - js/webpack/webpack.config.js
+++ Exercicios - js/webpack/webpack.config.js
@@ -1,12 +1,24 @@
+const modeDev = process.env.NODE_ENV !== "production";
 const webpack = require("webpack");
 const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const UglifyJsPlugin = require("uglifyjs-webpack-plugin");
+const OptimizeCSSAssetsPlugin = require("optimize-css-assets-webpack-plugin");
 
 module.exports = {
-  mode: "development",
+  mode: modeDev ? "development" : "production",
   entry: "./src/principal.js",
   output: {
     filename: "principal.js",
     path: __dirname + "/public",
+  },
+  optimization: {
+    minimizer: [
+      new UglifyJsPlugin({
+        cache: true,
+        parallel: true,
+      }),
+      new OptimizeCSSAssetsPlugin({}),
+    ],
   },
   plugins: [
     new MiniCssExtractPlugin({
