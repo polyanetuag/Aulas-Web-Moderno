45eb8f8d511dc6ad157ad16c59fdea5e535c3d5e
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-24 16:42:25 -0300

    script-regex do tooltip

--- Exercicios - js/bootstrap/exercicios/js/navegacao.js
+++ Exercicios - js/bootstrap/exercicios/js/navegacao.js
@@ -1,38 +1,38 @@
 (function () {
-    function navegarViaAjax(hash) {
-        if (!hash) return
+  function navegarViaAjax(hash) {
+    if (!hash) return;
 
-        const link = document.querySelector(`[wm-link='${hash}']`)
-        if(!link) return
+    const link = document.querySelector(`[wm-link='${hash}']`);
+    if (!link) return;
 
-        const destino = document.querySelector('[wm-link-destino]')
+    const destino = document.querySelector("[wm-link-destino]");
 
-        const url = hash.substring(1)
-        fetch(url)
-            .then(resp => resp.text())
-            .then(html => {
-                destino.innerHTML = html
-            })
+    const url = hash.substring(1);
+    fetch(url)
+      .then((resp) => resp.text())
+      .then((html) => {
+        destino.innerHTML = html;
+        eval(html.match(/\<script\>([\s\S]*)\<\/script\>/)[1]);
+      });
+  }
+
+  function configurarLinks() {
+    document.querySelectorAll("[wm-link]").forEach((link) => {
+      link.href = link.attributes["wm-link"].value;
+    });
+  }
+
+  function navegacaoInicial() {
+    if (location.hash) {
+      navegarViaAjax(location.hash);
+    } else {
+      const primeiroLink = document.querySelector("[wm-link]");
+      navegarViaAjax(primeiroLink.hash);
     }
+  }
 
-    function configurarLinks() {
-        document.querySelectorAll('[wm-link]')
-            .forEach(link => {
-                link.href = link.attributes['wm-link'].value
-            })
-    }
+  window.onhashchange = (e) => navegarViaAjax(location.hash);
 
-    function navegacaoInicial() {
-        if (location.hash) {
-            navegarViaAjax(location.hash)
-        } else {
-            const primeiroLink = document.querySelector('[wm-link]')
-            navegarViaAjax(primeiroLink.hash)
-        }
-    }
-
-    window.onhashchange = e => navegarViaAjax(location.hash)
-    
-    configurarLinks()
-    navegacaoInicial()
-})()
+  configurarLinks();
+  navegacaoInicial();
+})();
