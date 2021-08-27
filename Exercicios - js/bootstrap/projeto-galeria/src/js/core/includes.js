ffd0e80d1ad622d3d074aff3a241ad73ad2952bd
Polyane Tuag <pollyportugues@hotmail.com>
2021-08-27 18:05:43 -0300

    running the plugin

--- Exercicios - js/bootstrap/projeto-galeria/src/js/core/includes.js
+++ Exercicios - js/bootstrap/projeto-galeria/src/js/core/includes.js
@@ -1,4 +1,13 @@
 import $ from "jquery";
+import { onLoadHtmlSuccess } from "..core/includes";
+
+const loadHtmlSuccessCalbacks = []; //array de funções
+
+export function onLoadHtmlSuccess(callback) {
+  if (!loadHtmlSuccessCalbacks.includes(callback)) {
+    loadHtmlSuccessCalbacks.push(callback);
+  }
+}
 
 function loadIncludes(parent) {
   if (!parent) parent = "body";
@@ -12,6 +21,7 @@ function loadIncludes(parent) {
           $(e).html(data);
           $(e).removeAttr("wm-include");
 
+          loadHtmlSuccessCalbacks.forEach((callback) => callback(data));
           loadIncludes(e);
         },
       });
