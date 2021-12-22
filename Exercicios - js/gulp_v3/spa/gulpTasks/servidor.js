c6ed1fbdca76e1239e1965c927ee5382efc72afe
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-22 15:10:28 -0300

    implementing webserver build

--- Exercicios - js/gulp_v3/spa/gulpTasks/servidor.js
+++ Exercicios - js/gulp_v3/spa/gulpTasks/servidor.js
@@ -1,5 +1,20 @@
 const gulp = require("gulp");
+const watch = require("gulp-watch");
+const webserver = require("gulp-webserver");
 
-gulp.task("monitorarMudancas", () => {});
+gulp.task("monitorarMudancas", () => {
+  watch("src/**/*.html", () => gulp.start("app.html"));
+  watch("src/**/*.scss", () => gulp.start("app.css"));
+  watch("src/**/*.js", () => gulp.start("app.js"));
+  watch("src/assets/imgs/**/*.*", () => gulp.start("app.imgs"));
+});
 
-gulp.task("servidor", ["monitorarMudancas"], () => {});
+gulp.task("servidor", ["monitorarMudancas"], () => {
+  return gulp.src("build").pipe(
+    webserver({
+      livereload: true,
+      port: 8080,
+      open: true,
+    })
+  );
+});
