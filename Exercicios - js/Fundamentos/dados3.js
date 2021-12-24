4459c536722cca03e264c3449c7669946b6807d9
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-24 10:13:23 -0300

    working with data 3

---
+++ Exercicios - js/Fundamentos/dados3.js
@@ -0,0 +1,12 @@
+let idade = 31;
+console.log(typeof -31);
+console.log(typeof idade);
+
+let salario = 2895.4;
+console.log(typeof salario);
+
+let estaChovendo = true;
+console.log(typeof estaChovendo);
+
+console.log(typeof "Teste");
+console.log(typeof "Teste");
