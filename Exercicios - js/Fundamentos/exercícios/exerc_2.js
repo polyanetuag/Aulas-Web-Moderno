5624d460816024c90db69537331c798bd38f64ff
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-26 10:17:45 -0300

    exercise 2

---
+++ Exercicios - js/Fundamentos/exercícios/exerc_2.js
@@ -0,0 +1,9 @@
+// Escreva uma função que receba a idade de uma pessoa em anos e retorne a mesma idade em dias.
+
+function idadePessoa(idadeEmAnos) {
+  const diasDoAno = 365;
+
+  return diasDoAno * idadeEmAnos;
+}
+
+idadePessoa(10); // retorna 3650
