0686b4c60c5e52e184d49e66034c88e6b051534a
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-29 11:44:52 -0300

    exercise 10

---
+++ Exercicios - js/Fundamentos/exercícios/exerc_10.js
@@ -0,0 +1,11 @@
+// Elabore uma função que recebe um número como parâmetro e retorne uma string com o símbolo "+" na quantidade especificada no parâmetro.
+
+function simboloMais(quantidade) {
+  return Array(quantidade).fill("+").join("");
+}
+
+// OU
+
+function simboloMais(quantidade) {
+  return "+".repeat(quantidade);
+}
