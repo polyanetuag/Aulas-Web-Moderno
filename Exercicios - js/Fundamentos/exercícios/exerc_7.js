ca41be01a8bbc93c6cff162b205cd8bb0734b57f
Polyane Tuag <pollyportugues@hotmail.com>
2021-12-29 11:54:31 -0300

    fix exercise 7

--- Exercicios - js/Fundamentos/exercícios/exerc_7.js
+++ Exercicios - js/Fundamentos/exercícios/exerc_7.js
@@ -14,3 +14,8 @@ function estaEntre(minimo, maximo, numero, inclusivo = false) {
   if (inclusivo) return numero >= minimo && numero <= maximo;
   return numero > minimo && numero < maximo;
 }
+
+estaEntre(10, 100, 50); // retornará true
+estaEntre(16, 100, 160); // retornará false
+estaEntre(3, 150, 3); // retornará false
+estaEntre(3, 150, 3, true); // retornará true
